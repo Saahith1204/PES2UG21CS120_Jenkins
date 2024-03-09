76aabbe851d6c7e80125aece1af3ded5408a7c42
Saahith1204 <bsaahith@gmail.com>
2024-03-09 11:09:48 +0530

    Changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World (PES2UG21CS120)\n";
-  cout << "Hello, we're in Task-4"
+  cout << "Hello, we're in Task-4";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
